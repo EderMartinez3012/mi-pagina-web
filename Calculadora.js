879f43fc4261613dc1fd3722439240987cea239e
EderMartinez3012 <114781734+EderMartinez3012@users.noreply.github.com>
2025-10-02 15:49:53 -0500

    Update Calculadora.js

--- Calculadora.js
+++ Calculadora.js
@@ -1,1 +1,29 @@
+while (true) {
+    let num1 = parseFloat(prompt("Ingrese primer número"))
+    let num2 = parseFloat(prompt("Ingrese segundo número"))
 
+    let operacion = prompt("Ingresar operación")
+    
+    alert(realizarOperacion(num1, num2, operacion))
+    
+    let salir = prompt("¿Desea continuar?")
+
+    if (salir === "salir") {
+        break
+    } else{
+        
+    }
+    
+}
+
+function realizarOperacion(num1, num2, operacion) {
+    if (operacion === "suma") {
+        return num1 + num2
+    } else if (operacion === "resta") {
+        return num1 - num2
+    } else if (operacion === "multiplica") {
+        return num1 * num2
+    } else if (operacion === "divide") {
+        return num1 / num2
+    }
+}
